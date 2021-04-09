8d83793469a5fb08fe109a076353781e7f74651e
potetakoyaki <potetakoyaki@gmail.com>
2021-04-09 16:09:29 +0000

    [Upload]デバイス

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #トップ画面
+  root 'homes#top'
+  get 'homes/about' => 'homes#about'
+
+  #デバイス
+  devise_for :users
+
+  #ユーザー
+  resources :user
+
+  #投稿
+  resources :post
+
+  #コメント
+  resources :post_comment
 end
