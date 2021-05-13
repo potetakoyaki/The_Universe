9b03a5b99ec6443299d43017d7c97b94b44750f4
potetakoyaki <potetakoyaki@gmail.com>
2021-05-13 12:15:24 +0000

    [upldate]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   #投稿者ページ
   def show
     @user=User.find(params[:id])
+    if user_signed_in?
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
     if @user.id == current_user.id
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
         @room = Room.new
         @entry = Entry.new
       end
+    end
     end
   end
 
